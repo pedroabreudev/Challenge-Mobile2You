82210635ab315d2c5c29bea2e06e9a9bcc1023d1
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-09 08:58:13 -0400

    Created Interface MovieService

---
+++ app/src/main/java/br/com/pedroabreudev/desafio_mobile2you/service/repository/remote/MovieService.kt
@@ -0,0 +1,20 @@
+package br.com.pedroabreudev.desafio_mobile2you.service.repository.remote
+
+import br.com.pedroabreudev.desafio_mobile2you.service.model.Movie
+import br.com.pedroabreudev.desafio_mobile2you.service.model.SimilarMovieList
+import retrofit2.Call
+import retrofit2.http.GET
+
+private const val api_key = "418fc16ab5943f1423c436aed898d978"
+private const val movie_id = "249660"
+
+interface MovieService {
+
+    @GET("/movie/$movie_id?api_key=$api_key")
+    fun getMovieDetails(): Call<Movie>
+
+    @GET("/movie/$movie_id/similar?api_key=$api_key")
+    fun getSimilarMovie(): Call<SimilarMovieList>
+
+
+}
